e0c387eeb62cb2151816ba2a2cca2b3fb7963c02
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-04-17 20:21:36 -0400

    Implement barebones connection to socket.io server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
+  const [socketId, setSocketId] = useState('');
+  
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+    
+    socket.on("connect", () => {
+      setSocketId(socket.id);
+    });
+  }, []);
+
+
   return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+      <div className="App">
+        {`Connected: ${socketId}`}
+      </div>
+    );
 }
 
 export default App;
