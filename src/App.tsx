bb91b8d555969ab8d53827dd0b496656e6626dff
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-04-24 22:11:09 -0400

    Implement auto room creation for private messaging

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,25 @@ type Message = {
   message: string,
 };
 
+type PrivateMessage = {
+  roomId: string,
+  senderId: string,
+  message: string,
+};
+
 const OnlineUsersStatus = RtRecord({
   userIds: RtArray(RtString),
 });
 
 function App() {
   const socket = useRef<Socket>();
-
+  const [viewingPrivateMessages, setViewingPrivateMessages] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
+  const [privateMessages, setPrivateMessages] = useState<PrivateMessage[]>([]);
+  const [rooms, setRooms] = useState<string[]>([]);
   const [socketId, setSocketId] = useState('');
   const [userSocketIds, setUserSocketIds] = useState<string[]>([])
+  const [currentRoom, setCurrentRoom] = useState<string>('');
 
   const {
     register,
@@ -45,13 +54,24 @@ function App() {
 
   const onSubmit: SubmitHandler<Input> = ({ message }) => {
     if (socket.current) {
-      setMessages(
-        messages.concat({
-          senderId: socketId,
-          message
-        })
-      );
-      socket.current.emit('message', socketId, message);
+      if (!currentRoom) {
+        setMessages(
+          messages.concat({
+            senderId: socketId,
+            message
+          })
+        );
+        socket.current.emit('message', socketId, message);
+      } else {
+        setPrivateMessages(
+          privateMessages.concat({
+            roomId: currentRoom,
+            senderId: socketId,
+            message
+          })
+        );
+        socket.current.emit('private message', currentRoom, message);
+      }
     }
 
     reset({
@@ -91,6 +111,29 @@ function App() {
       }
     })
 
+    socket.current.on('private message', (roomId, userSocketId, message) => {
+      if (socket.current) {
+        const payload: PrivateMessage = {
+          roomId: roomId,
+          senderId: userSocketId,
+          message,
+        };
+        setPrivateMessages((privateMessages) => privateMessages.concat(payload));
+      }
+
+    });
+
+    socket.current.on('friend request', (userSocketId, roomId) => {
+      setRooms((rooms) => rooms.concat(roomId));
+
+      console.log(`new room [${roomId}] initialized with ${userSocketId}`);
+    });
+
+    socket.current.on('add room', (roomId) => {
+      setRooms((rooms) => rooms.concat(roomId));
+      socket.current?.emit('join room', roomId);
+    });
+
     socket.current.on('disconnect', () => {
       console.log("disconnected from socket");
     });
@@ -98,18 +141,31 @@ function App() {
     return () => { socket.current?.disconnect(); };
   }, []);
 
-  // const handleReceiverChange = (id: string) => {
-  //   setCurrentRoomId(id);
-  //   console.log(`Now talking to ${id}`);
-  // }
+  const handleRoomChange = (roomId: string) => {
+    setCurrentRoom(roomId);
+    console.log(`Now talking in room ${roomId}`);
+    setViewingPrivateMessages(true);
+  };
+
+  const handleFriendRequest = (userId: string) => {
+    socket.current?.emit('friend request', userId);
+  };
 
   return (
       <div className="flex flex-row">
-        <div className="outline outline-1 h-screen p-1">
+        <div className="outline outline-1 h-screen p-1 min-w-fit">
           <ul>
             {userSocketIds.map((id, i) => ( 
               <li key={i} className="odd:bg-white even:bg-slate-200">
-                <button type="button">{id}</button>
+                <button type="button" onClick={() => handleFriendRequest(id)}>{id}</button>
+              </li>
+            ))}
+          </ul>
+          <span className="outline">Rooms</span>
+          <ul>
+            {rooms.map((room, i) => (
+              <li key={i}>
+                <button type="button" onClick={() => handleRoomChange(room)}>{room}</button>
               </li>
             ))}
           </ul>
@@ -118,13 +174,24 @@ function App() {
           <p className="bg-slate-100">
             {`Connected as: ${socketId}`}
           </p>
-          <ul>
+          <p className="bg-slate-100">
+            {`Room: ${currentRoom}`}
+          </p>
+          {!viewingPrivateMessages && <ul>
             {messages.map((m, i) => (
               <li key={i} className="p-1 odd:bg-white even:bg-slate-100">
                 {`${m.senderId} | ${m.message}`}
               </li>
             ))}
-          </ul>
+          </ul>}
+          {viewingPrivateMessages && <ul>
+            {privateMessages.map((m, i) => (
+              currentRoom === m.roomId &&
+              <li key={i} className="p-1 odd:bg-white even:bg-slate-100">
+                {`${m.roomId} | ${m.senderId} | ${m.message}`}
+              </li>
+            ))}
+          </ul>}
           <form className="flex h-12 mt-auto" onSubmit={handleSubmit(onSubmit)}>
             <input className="grow p-1 center shadow-lg rounded-lg outline outline-1 hover:outline-2" {...register('message')} />
             <button className="rounded-lg shadow-lg ml-1 p-1 outline outline-1 hover:outline-2" type="submit">Send</button>
