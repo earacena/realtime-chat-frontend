c7cbcf389f8eecb420588aea47d39cfc6c14b0a7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-01 22:14:37 -0400

    Implement store and store types

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { messagesReducer } from './features/Message';
+import { roomsReducer } from './features/Room';
+import { socketReducer } from './features/Socket';
 
-const store = configureStore({ reducer: {} });
+const store = configureStore({ 
+  reducer: {
+    socket: socketReducer,
+    messages: messagesReducer,
+    rooms: roomsReducer,
+  }
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
