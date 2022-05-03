f8082ffd108bdb44c4e1055a56dbebb1fab66313
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-02 22:22:07 -0400

    Add users state to store

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { messagesReducer } from './features/Message';
 import { roomsReducer } from './features/Room';
 import { socketReducer } from './features/Socket';
+import { usersReducer } from './features/UserList';
 
 const store = configureStore({ 
   reducer: {
     socket: socketReducer,
     messages: messagesReducer,
     rooms: roomsReducer,
+    users: usersReducer,
   }
 });
 
