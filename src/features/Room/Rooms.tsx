bb3c9c9ed204e6d49ce580d718a0f18e5898f23b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-03 21:57:05 -0400

    Refactor Rooms to state actions and selector

--- src/features/Room/Rooms.tsx
+++ src/features/Room/Rooms.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import type { Room, Rooms as RoomArray } from '../../app.types';
+import { setCurrentRoom } from '../Room';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 
-interface RoomsProps {
-  rooms: RoomArray;
-  handleRoomChange: (roomId: Room) => void;
-}
-
-function Rooms({ rooms, handleRoomChange }: RoomsProps) {
+function Rooms() {
+  const dispatch = useAppDispatch();
+  const rooms = useAppSelector((state) => state.rooms.allRooms);
+  
   return (
     <div className="flex flex-col">
       <div className="flex border-b-2">
@@ -15,7 +14,7 @@ function Rooms({ rooms, handleRoomChange }: RoomsProps) {
       <ul>
         {rooms.map((room, i) => (
           <li key={room.roomId}>
-            <button type="button" onClick={() => handleRoomChange(room)}>{room.roomName}</button>
+            <button type="button" onClick={() => dispatch(setCurrentRoom({ room }))}>{room.roomName}</button>
           </li>
         ))}
       </ul>
