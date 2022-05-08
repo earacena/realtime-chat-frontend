3fcd58f17e86e3eab085404c127e4f0c707f39b9
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-07 22:32:50 -0400

    Implement chat middleware for socket operations

---
+++ src/features/Chat/middleware/chat.middleware.ts
@@ -0,0 +1,67 @@
+import { Middleware } from '@reduxjs/toolkit';
+import { setMessages, addMessage, startConnecting, connectionEstablished } from '../stores/chat.slice';
+import { io, Socket } from 'socket.io-client';
+import { removeConnectedUserId, setConnectedUserIds } from '../../UserList';
+import { addRoom, setUserIdsInPrivateRoom } from '../../Room';
+import type { Message, Messages } from '../types/chat.types';
+
+const url = 'http://localhost:3001/';
+
+const chatMiddleware: Middleware = store => {
+  let socket: Socket;
+
+  return next => action => {
+    const isConnectionEstablished = socket && store.getState().chat.isConnected;
+
+
+    if (startConnecting.match(action)) {
+      // Initialize socket connection if appropriate action received
+      socket = io(url);
+
+      const userConnectionHandler = (userSocketId: string) => {
+        store.dispatch(setConnectedUserIds({ userId: userSocketId }));
+      };
+
+      const userDisconnectionHandler = (userSocketId: string) => {
+        store.dispatch(removeConnectedUserId({ id: userSocketId }));
+      };
+
+      const connectedUserListHandler = (allUserSocketIds: string[]) => {
+        store.dispatch(setConnectedUserIds({ connectedUserIds: allUserSocketIds }));
+      };
+
+      const receiveMessageHandler = (message: Message) => {
+        store.dispatch(addMessage(message));
+      };
+      
+      const receiveAllMessagesHandler = (messages: Messages) => {
+        store.dispatch(setMessages (messages));
+      };
+
+      const privateRoomRequestHandler = (userSocketId: string, roomId: string) => {
+        store.dispatch(addRoom({ room: { roomId, roomName: `chat with ${userSocketId}` } }));
+        socket.emit('join room', roomId);
+    
+        console.log(`new room [${roomId}] initialized with ${userSocketId}`);
+        store.dispatch(setUserIdsInPrivateRoom({ userId: userSocketId }));
+      };
+
+      const connectionHandler = () => {
+        store.dispatch(connectionEstablished());
+      };   
+
+      const disconnectionHandler = () => {
+        console.log("disconnected from socket");
+      };
+
+      socket.on('connect', connectionHandler);
+      socket.on('user connected', userConnectionHandler);
+      socket.on('user disconnected', userDisconnectionHandler); 
+      socket.on('all connected users', connectedUserListHandler);
+      socket.on('receive message', receiveMessageHandler);
+      socket.on('receive all messages', receiveAllMessagesHandler);
+      socket.on('private room request', privateRoomRequestHandler); 
+      socket.on('disconnect', disconnectionHandler);
+    }
+  }
+}
