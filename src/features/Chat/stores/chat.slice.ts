bc60401149a89ee3a60be556643d3f9eef243be7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-08 16:04:00 -0400

    Store socket id in ChatState and implement action

--- src/features/Chat/stores/chat.slice.ts
+++ src/features/Chat/stores/chat.slice.ts
@@ -1,13 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { Message } from '../../../app.types';
+import { Messages } from '../types/chat.types';
 
 interface ChatState {
-  messages: Message[];
+  socketId: string;
+  messages: Messages;
   isConnecting: boolean;
   isConnected: boolean;
 };
 
 const initialState: ChatState = {
+  socketId: '',
   messages: [],
   isConnecting: false,
   isConnected: false,
@@ -20,6 +22,7 @@ const chatSlice = createSlice({
     startConnecting: (state) => ({ ...state, isConnecting: true }),
     connectionEstablished: (state) => ({ ...state, isConnecting: false, isConnected: true }),
     disconnected: (state) => ({ ...state, isConnected: false }),
+    setSocketId: (state, action) => ({ ...state, socketId: action.payload.socketId}),
     setMessages: (state, action) => ({ ...state, messages:  action.payload.messages}),
     addMessage: (state, action) => ({ ...state, messages: state.messages.concat(action.payload.message)}),
     sendMessage: (state, action) => { return; },
@@ -30,6 +33,7 @@ export const {
   startConnecting,
   connectionEstablished,
   disconnected,
+  setSocketId,
   setMessages,
   addMessage,
   sendMessage,
