460ce5e13900b8f6173ee06740ce43194a25136f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-20 22:37:44 -0400

    Add LoginForm and make Chat visible after login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Chat } from './features/Chat';
 import { SideBar } from './features/SideBar';
+import { LoginForm } from './features/Login';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="flex flex-row">
-      <SideBar />
-      <Chat />
+      <LoginForm />
+      {loggedIn && <SideBar />}
+      {loggedIn && <Chat />}
     </div>
   );
 }
