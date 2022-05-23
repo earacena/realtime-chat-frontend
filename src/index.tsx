51d75bd5c68a42c0decdef94bc4dc77231b14569
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-22 21:26:54 -0400

    Add BrowserRouter provider and StrictMode to App

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
