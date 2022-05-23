9271f548c52dc230d9f916c11fdb969d7356dc66
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-22 21:07:32 -0400

    Implement auth slice

---
+++ src/features/Login/stores/auth.slice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AuthState, TokenPayload, AuthenticatedUserPayload } from '../types/auth.types';
+
+
+const initialState: AuthState = {
+  token: '',
+  user: {
+    id: 0,
+    username: '',
+    name: '',
+  },
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setToken: (state: AuthState, action: PayloadAction<TokenPayload>) => ({ ...state, token: action.payload.token }),
+    setAuthenticatedUser: (state: AuthState, action: PayloadAction<AuthenticatedUserPayload>) => ({ ...state, user: action.payload.user }),
+  }
+});
+
+export const {
+  setToken,
+  setAuthenticatedUser,
+} = authSlice.actions;
+
+export default authSlice.reducer;
