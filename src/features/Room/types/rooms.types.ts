8538bbf0c0505cebd04f0d4f06e66062620900dd
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-22 20:39:02 -0400

    Move Room feature types into their own file

---
+++ src/features/Room/types/rooms.types.ts
@@ -0,0 +1,40 @@
+import {
+  String as RtString,
+  Static as RtStatic,
+  Array as RtArray,
+  Record as RtRecord,
+} from 'runtypes';
+
+export const RoomType = RtRecord({
+  roomId: RtString,
+  roomName: RtString,
+});
+export type Room = RtStatic<typeof RoomType>;
+export const RoomArray = RtArray(RoomType);
+export type Rooms = RtStatic<typeof RoomArray>;
+
+export interface RoomsState {
+  allRooms: Rooms;
+  currentRoom: Room;
+  userIdsInPrivateRoom: string[];
+}
+
+export type allRoomsPayload = {
+  allRooms: Rooms,
+};
+
+export type RoomPayload = {
+  room: Room,
+};
+
+export type CurrentRoomPayload = {
+  currentRoom: Room,
+};
+
+export type UserIdPayload = {
+  userId: string,
+};
+
+export type UserIdsInPrivateRoomPayload = {
+  userIdsInPrivateRoom: string[],
+}
