afcda1e828c220f46bf80278d54f86f54d1ead9e
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-24 19:30:54 -0400

    Implement routes for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import { LoginForm, RegisterForm } from './features/Login';
 import { Chat } from './features/Chat';
-import { SideBar } from './features/SideBar';
-import { LoginForm } from './features/Login';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      {!loggedIn && <LoginForm />}
-      {loggedIn && <SideBar />}
-      {loggedIn && <Chat />}
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
     </div>
   );
 }
