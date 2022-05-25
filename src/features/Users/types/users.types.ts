bdb177c79895b223c5bb9c9953cbe09508ae6b6e
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-24 21:36:41 -0400

    Add User and User service types

--- src/features/Users/types/users.types.ts
+++ src/features/Users/types/users.types.ts
@@ -1,6 +1,21 @@
+import {
+  Number as RtNumber,
+  String as RtString,
+  Union as RtUnion,
+  InstanceOf as RtInstanceOf,
+  Record as RtRecord,
+  Array as RtArray,
+} from 'runtypes';
+
 export interface UsersState {
   connectedUserIds: string[];
   userIdsInPrivateRoom: string[];
+};
+
+export interface CreateUserFields {
+  name: string;
+  username: string;
+  password: string;
 };
 
 export type ConnectedUserIdsPayload = {
@@ -10,3 +25,16 @@ export type ConnectedUserIdsPayload = {
 export type UserIdPayload = {
   userId: string;
 };
+
+export const UserType = RtRecord({
+  id: RtNumber,
+  name: RtString,
+  username: RtString,
+  passwordHash: RtString,
+  dateRegistered: RtUnion(
+    RtInstanceOf(Date),
+    RtString.withConstraint((x: string) => (x || x !== null || typeof x === 'string' || !Number.isNaN(Date.parse(x)))),
+  ),
+});
+
+export const UserTypeArray = RtArray(UserType);
