87e76592e96171a33ad518d60be188ff89f0e97d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-25 23:39:54 -0400

    Re-export user service from users feature folder

--- src/features/Users/index.ts
+++ src/features/Users/index.ts
@@ -2,3 +2,5 @@ export { default as UserList } from './components/UserList';
 
 export { default as usersReducer } from './stores/users.slice';
 export * from './stores/users.slice';
+
+export { default as userService } from './api/user.service';
