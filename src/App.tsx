91d674e33dcfa861272c5b82f3f8e3f274f59393
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-27 19:46:12 -0400

    Add Notification to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { LoginForm, RegisterForm } from './features/Login';
 import { Chat } from './features/Chat';
 import { Route, Routes } from 'react-router-dom';
+import { Notification } from './features/Notification';
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      <Notification />
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/login" element={<LoginForm />} />
