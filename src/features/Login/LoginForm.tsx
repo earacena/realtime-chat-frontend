79361d677ca45fbe82aa1df6f78294a57ba31068
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-26 22:23:36 -0400

    Add Notification to LoginForm

--- src/features/Login/LoginForm.tsx
+++ src/features/Login/LoginForm.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { FormWrapper, LabelErrorMessage } from '../../components';
 import loginService from './api/login.service';
 import { setAuthenticatedUser } from './stores/auth.slice';
+import { Notification } from '../Notification';
 
 type Input = {
   username: string;
@@ -51,48 +52,51 @@ function LoginForm() {
   
 
   return (
-    <FormWrapper>
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-        <label className="mt-3" htmlFor="username-input">Username</label>
-        {errors.username && <LabelErrorMessage content="Required" />}
-        <input
-          id="username-input"
-          aria-label="username"
-          className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
-          type="text"
-          placeholder=""
-          {...register('username', { required: true })}
-        />
-        <label htmlFor="password-input">Password</label>
-        {errors.password && <LabelErrorMessage content="Required" />}
-        <input
-          id="password-input"
-          aria-label="password"
-          className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
-          type="password"
-          placeholder=""
-          {...register('password', { required: true })}
-        />
-        <button 
-          id="login-button"
-          className="rounded-md p-3 bg-slate-500 text-white w-full mt-2 hover:bg-slate-400"
-          type="submit"
-          aria-label="login"
-        >
-          Login
-        </button>
-        <p className="mt-4 text-sm self-center text-slate-600">Don't have an account?</p>
-        <button
-          id="register-button"
-          className="rounded-md p-3 outline outline-2 text-slate-600 w-full mt-1 hover:bg-slate-200"
-          type="button"
-          aria-label="login"
-          onClick={registerButtonClicked}
-        >
-          Create New Account
-        </button>
-      </form>
-    </FormWrapper>
+    <div>
+      <Notification />
+      <FormWrapper>
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <label className="mt-3" htmlFor="username-input">Username</label>
+          {errors.username && <LabelErrorMessage content="Required" />}
+          <input
+            id="username-input"
+            aria-label="username"
+            className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
+            type="text"
+            placeholder=""
+            {...register('username', { required: true })}
+          />
+          <label htmlFor="password-input">Password</label>
+          {errors.password && <LabelErrorMessage content="Required" />}
+          <input
+            id="password-input"
+            aria-label="password"
+            className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
+            type="password"
+            placeholder=""
+            {...register('password', { required: true })}
+          />
+          <button 
+            id="login-button"
+            className="rounded-md p-3 bg-slate-500 text-white w-full mt-2 hover:bg-slate-400"
+            type="submit"
+            aria-label="login"
+          >
+            Login
+          </button>
+          <p className="mt-4 text-sm self-center text-slate-600">Don't have an account?</p>
+          <button
+            id="register-button"
+            className="rounded-md p-3 outline outline-2 text-slate-600 w-full mt-1 hover:bg-slate-200"
+            type="button"
+            aria-label="login"
+            onClick={registerButtonClicked}
+          >
+            Create New Account
+          </button>
+        </form>
+      </FormWrapper>
+    </div>
   )
 }
 
