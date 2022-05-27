57f722c142ba0ea1b48d91f2c0c62db113995927
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-26 20:10:56 -0400

    Add auth state reducer to store

--- src/store.ts
+++ src/store.ts
@@ -3,12 +3,14 @@ import { chatReducer } from './features/Chat';
 import chatMiddleware from './features/Chat/middleware/chat.middleware';
 import { roomsReducer } from './features/Rooms';
 import { usersReducer } from './features/Users';
+import { authReducer } from './features/Login/';
 
 const store = configureStore({ 
   reducer: {
     chat: chatReducer,
     rooms: roomsReducer,
     users: usersReducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat([chatMiddleware]);
