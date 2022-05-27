9b2604736b6280826a6a7d10661e63c731ae6902
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-26 20:10:27 -0400

    Re-export auth state reducer

--- src/features/Login/index.ts
+++ src/features/Login/index.ts
@@ -1,4 +1,6 @@
 export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './RegisterForm';
 
+export { default as authReducer } from './stores/auth.slice';
+
 export { default as loginService } from './api/login.service';
