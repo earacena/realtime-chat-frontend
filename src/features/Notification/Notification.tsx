53748f8392ef4603928f0e66c749d147838c6157
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-27 20:50:06 -0400

    Fix default import error by removing notify helper
    The 'notify' helper function caused the runtime error as a result of
    the store import needed to access dispatch.

--- src/features/Notification/Notification.tsx
+++ src/features/Notification/Notification.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import { useAppSelector } from '../../hooks';
-import store from '../../store';
 import { setNotification, resetNotification } from './stores/notification.slice';
 
 function Notification() {
   const { type, message } = useAppSelector((state) => state.notification);
   
+  const baseNotificationStyle = `{message === undefined ? 'hidden' : 'flex'}`;
+  const messageNotificationStyle = baseNotificationStyle.concat('');
+  const errorNotificationStyle = baseNotificationStyle.concat('');
+  const notificationStyle = (type === 'error') ? errorNotificationStyle : messageNotificationStyle;
+
   return (
-    <div>
+    <div className={notificationStyle}>
       {message}
     </div>
   );
 }
 
-export const notify = (
-  type: string,
-  message: string,
-  durationInSeconds: number,
-) => {
-  const { dispatch } = store;
-  const newTimeoutId = setTimeout(() => {
-    dispatch(resetNotification());
-  }, durationInSeconds * 1000);
-  dispatch(setNotification({ type, message, timeoutId: newTimeoutId }));
-};
 
 export default Notification;
