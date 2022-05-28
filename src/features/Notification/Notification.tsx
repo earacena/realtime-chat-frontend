6467ea0813e0de59614314605f83e2aca9a50d9d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-27 22:46:29 -0400

    Implement base style and auto hiding

--- src/features/Notification/Notification.tsx
+++ src/features/Notification/Notification.tsx
@@ -5,13 +5,14 @@ import { setNotification, resetNotification } from './stores/notification.slice'
 function Notification() {
   const { type, message } = useAppSelector((state) => state.notification);
   
-  const baseNotificationStyle = `{message === undefined ? 'hidden' : 'flex'}`;
+  const baseNotificationStyle = 'outline outline-1 outline-red-500 bg-red-100 p-3 mb-2 rounded shadow-lg';
   const messageNotificationStyle = baseNotificationStyle.concat('');
   const errorNotificationStyle = baseNotificationStyle.concat('');
   const notificationStyle = (type === 'error') ? errorNotificationStyle : messageNotificationStyle;
+  const style = (message === '') ? 'hidden' : notificationStyle;
 
   return (
-    <div className={notificationStyle}>
+    <div className={style}>
       {message}
     </div>
   );
