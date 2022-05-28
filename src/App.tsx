22a9031712e5a2b05216452b6ae74cfe0913d4ac
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-27 21:30:45 -0400

    Make Chat the component at route '/'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { LoginForm, RegisterForm } from './features/Login';
 import { Chat } from './features/Chat';
-import { Route, Routes } from 'react-router-dom';
 import { Notification } from './features/Notification';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="flex flex-col items-center justify-center h-screen">
       <Notification />
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<Chat />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/chat" element={<Chat />} />
