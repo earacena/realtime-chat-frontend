9e16d2274d14f6b89e066b31559357fc92c7efbf
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-27 21:30:17 -0400

    Remove unneeded div

--- src/features/Login/LoginForm.tsx
+++ src/features/Login/LoginForm.tsx
@@ -49,50 +49,48 @@ function LoginForm() {
   
 
   return (
-    <div>
-      <FormWrapper>
-        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-          <label className="mt-3" htmlFor="username-input">Username</label>
-          {errors.username && <LabelErrorMessage content="Required" />}
-          <input
-            id="username-input"
-            aria-label="username"
-            className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
-            type="text"
-            placeholder=""
-            {...register('username', { required: true })}
-          />
-          <label htmlFor="password-input">Password</label>
-          {errors.password && <LabelErrorMessage content="Required" />}
-          <input
-            id="password-input"
-            aria-label="password"
-            className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
-            type="password"
-            placeholder=""
-            {...register('password', { required: true })}
-          />
-          <button 
-            id="login-button"
-            className="rounded-md p-3 bg-slate-500 text-white w-full mt-2 hover:bg-slate-400"
-            type="submit"
-            aria-label="login"
-          >
-            Login
-          </button>
-          <p className="mt-4 text-sm self-center text-slate-600">Don't have an account?</p>
-          <button
-            id="register-button"
-            className="rounded-md p-3 outline outline-2 text-slate-600 w-full mt-1 hover:bg-slate-200"
-            type="button"
-            aria-label="login"
-            onClick={registerButtonClicked}
-          >
-            Create New Account
-          </button>
-        </form>
-      </FormWrapper>
-    </div>
+    <FormWrapper>
+      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+        <label className="mt-3" htmlFor="username-input">Username</label>
+        {errors.username && <LabelErrorMessage content="Required" />}
+        <input
+          id="username-input"
+          aria-label="username"
+          className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
+          type="text"
+          placeholder=""
+          {...register('username', { required: true })}
+        />
+        <label htmlFor="password-input">Password</label>
+        {errors.password && <LabelErrorMessage content="Required" />}
+        <input
+          id="password-input"
+          aria-label="password"
+          className="p-2 mt-1 mb-3 bg-slate-200 rounded-sm focus:outline-slate-400"
+          type="password"
+          placeholder=""
+          {...register('password', { required: true })}
+        />
+        <button 
+          id="login-button"
+          className="rounded-md p-3 bg-slate-500 text-white w-full mt-2 hover:bg-slate-400"
+          type="submit"
+          aria-label="login"
+        >
+          Login
+        </button>
+        <p className="mt-4 text-sm self-center text-slate-600">Don't have an account?</p>
+        <button
+          id="register-button"
+          className="rounded-md p-3 outline outline-2 text-slate-600 w-full mt-1 hover:bg-slate-200"
+          type="button"
+          aria-label="login"
+          onClick={registerButtonClicked}
+        >
+          Create New Account
+        </button>
+      </form>
+    </FormWrapper>
   )
 }
 
