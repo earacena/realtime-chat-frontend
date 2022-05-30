41b531540eee4ff179698fed56d2f294100e00ae
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-29 22:19:41 -0400

    Fix SideBar and Chat positioning and routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,27 @@ import { Route, Routes } from 'react-router-dom';
 import { LoginForm, RegisterForm } from './features/Login';
 import { Chat } from './features/Chat';
 import { Notification } from './features/Notification';
+import { SideBar } from './features/SideBar';
 
 function App() {
+
+  const chatWithSideBar = () => {
+    return (
+      <div className="flex flex-row">
+        <SideBar />
+        <Chat />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Notification />
       <Routes>
-        <Route path="/" element={<Chat />} />
+        <Route path="/" element={chatWithSideBar()} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/chat" element={chatWithSideBar()} />
       </Routes>
     </div>
   );
