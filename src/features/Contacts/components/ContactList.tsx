1608e19f9ef054259e2523a74e1e1b67b4f4ca76
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-30 23:07:46 -0400

    Add ContactFinderDialog to ContactList

--- src/features/Contacts/components/ContactList.tsx
+++ src/features/Contacts/components/ContactList.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector } from '../../../hooks';
+import { BsFillPersonPlusFill } from 'react-icons/bs';
+import ContactFinderDialog from './ContactFinder';
 
 function ContactList() {
-  const contacts = useAppSelector((state) => state.user.contacts);
+  // const contacts = useAppSelector((state) => state.user.contacts);
+  const [isContactFinderOpen, setIsContactFinderOpen] = useState(false);
+
+  const addContactButtonPressed = () => setIsContactFinderOpen(true);
 
   return (
-    <ul>
-      {contacts.map((contact) => contact.name)}
-    </ul>
+    <div>
+      <ContactFinderDialog isOpen={isContactFinderOpen} setIsOpen={setIsContactFinderOpen} />
+
+      <button 
+        id="find-contact-button"
+        className="rounded-md p-3 bg-slate-500 text-white w-full mt-3 hover:bg-slate-400"
+        type="button"
+        aria-label="find"
+        onClick={addContactButtonPressed}
+      >
+        <BsFillPersonPlusFill />
+      </button>
+
+      <ul>
+      </ul>
+    </div>
   );
 }
 
