e3b223af159e53b9890f3c6d3f7033ce3df3924c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-31 20:41:27 -0400

    Make ContactList the only component in SideBar

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { UserList } from '../../features/Users/';
-import { Rooms } from '../../features/Rooms/';
+import { ContactList } from '../Contacts';
 
 function SideBar() {
   return (
     <div className="outline outline-1 h-screen p-1 w-96">
-      <UserList />
-      <Rooms />
+      <ContactList />
     </div>
   );
 }
