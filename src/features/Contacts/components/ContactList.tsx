283beb4d9c3b64d116ada1762ad06a478400758c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-31 20:40:01 -0400

    Restyle button and rename ContactFinder

--- src/features/Contacts/components/ContactList.tsx
+++ src/features/Contacts/components/ContactList.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { useAppSelector } from '../../../hooks';
 import { BsFillPersonPlusFill } from 'react-icons/bs';
-import ContactFinderDialog from './ContactFinder';
+import ContactRequestDialog from './ContactRequestDialog';
 
 function ContactList() {
   // const contacts = useAppSelector((state) => state.user.contacts);
@@ -11,16 +10,17 @@ function ContactList() {
 
   return (
     <div>
-      <ContactFinderDialog isOpen={isContactFinderOpen} setIsOpen={setIsContactFinderOpen} />
+      <ContactRequestDialog isOpen={isContactFinderOpen} setIsOpen={setIsContactFinderOpen} />
 
       <button 
         id="find-contact-button"
-        className="rounded-md p-3 bg-slate-500 text-white w-full mt-3 hover:bg-slate-400"
+        className="flex rounded-md p-3 bg-slate-500 text-white w-full mt-3 hover:bg-slate-400 items-center"
         type="button"
         aria-label="find"
         onClick={addContactButtonPressed}
       >
-        <BsFillPersonPlusFill />
+        <BsFillPersonPlusFill size={20} className="mx-2"/>
+        <span className="mx-7"> Add Contact </span>
       </button>
 
       <ul>
