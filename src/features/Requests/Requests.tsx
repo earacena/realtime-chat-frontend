470ba17e13afcc725b6a36367a191ad79a13e79a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-01 21:52:20 -0400

    Implement empty Requests component

---
+++ src/features/Requests/Requests.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+function Requests() {
+
+  return (
+    <div>
+      <span className="text-xl">Requests</span>
+      <ul>
+
+      </ul>
+    </div>
+  );
+}
+
+export default Requests;
