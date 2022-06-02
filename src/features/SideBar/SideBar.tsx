7cbe33190eaa25829a5290acc2f05154eb3f0b62
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-01 21:52:43 -0400

    Add Requests component to SideBar

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ContactList } from '../Contacts';
+import Requests from '../Requests';
 
 function SideBar() {
   return (
     <div className="outline outline-1 h-screen p-1 w-96">
+      <Requests />
       <ContactList />
     </div>
   );
