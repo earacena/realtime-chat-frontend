8682ddbeb32dfb19658434e4c01dd9193854548a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-02 21:46:49 -0400

    Implement initial fetching of requests in hook

--- src/features/Requests/Requests.tsx
+++ src/features/Requests/Requests.tsx
@@ -1,12 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAppSelector } from '../../hooks';
+import requestService from './api/request.service';
+import { Requests as RequestArray } from './types/requests.types';
 
 function Requests() {
+  const userId = useAppSelector((state) => state.auth.user.id);
+  const [requests, setRequests] = useState<RequestArray>([]);
+
+  useEffect(() => {
+    const fetchRequests = async () => {
+      try {
+        const fetchedRequests = await requestService.getRequestsOfUser(userId);
+        setRequests(fetchedRequests);
+  
+      } catch (error: unknown) {
+        console.error(error);
+      }
+    }
+
+    fetchRequests();
+  }, [userId]);
 
   return (
     <div>
       <span className="text-xl">Requests</span>
       <ul>
-
+        {requests.map((r) => r.id)}
       </ul>
     </div>
   );
