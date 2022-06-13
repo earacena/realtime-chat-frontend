4fbfdb6356c5d551d9cda37aaddaca3606b8b17b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-12 21:32:17 -0400

    Refactor Requests to use requests state actions

--- src/features/Requests/Requests.tsx
+++ src/features/Requests/Requests.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useAppSelector } from '../../hooks';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { setRequests } from './stores/request.slice';
 import requestService from './api/request.service';
-import { Requests as RequestArray } from './types/requests.types';
 import RequestCard from './components/RequestCard';
 
 function Requests() {
+  const dispatch = useAppDispatch();
   const userId = useAppSelector((state) => state.auth.user.id);
-  const [requests, setRequests] = useState<RequestArray>([]);
+  const requests = useAppSelector((state) => state.requests.requests);
 
   useEffect(() => {
     const fetchRequests = async () => {
       try {
         const fetchedRequests = await requestService.getRequestsOfUser(userId);
-        setRequests(fetchedRequests);
+        dispatch(setRequests({ requests: fetchedRequests }));
   
       } catch (error: unknown) {
         console.error(error);
@@ -20,7 +21,7 @@ function Requests() {
     }
 
     fetchRequests();
-  }, [userId]);
+  }, [dispatch, userId]);
 
   return (
     <div>
