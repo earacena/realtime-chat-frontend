cb0454023609048890665c493ca4b318a879cf66
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-12 21:31:22 -0400

    Re-export requests reducer and action

--- src/features/Requests/index.ts
+++ src/features/Requests/index.ts
@@ -1,2 +1,5 @@
 export { default as requestService } from './api/request.service';
 export { default as Requests } from './Requests';
+
+export { default as requestsReducer } from './stores/request.slice';
+export { setRequests } from './stores/request.slice';
