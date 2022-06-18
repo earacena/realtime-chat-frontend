7ff4456c4b474ecc4b295caed8f1a4cc40889930
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-17 20:29:19 -0400

    Implement hook for retrieving user contacts

--- src/features/Contacts/components/ContactList.tsx
+++ src/features/Contacts/components/ContactList.tsx
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsFillPersonPlusFill } from 'react-icons/bs';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { setContacts, userService } from '../../Users';
+import { UserDetails } from '../../Users/types/users.types';
 import ContactRequestDialog from './ContactRequestDialog';
 
 function ContactList() {
-  // const contacts = useAppSelector((state) => state.user.contacts);
+  const dispatch = useAppDispatch();
+
+  const userId = useAppSelector((state) => state.auth.user.id);
+  const contacts = useAppSelector((state) => state.users.contacts);
   const [isContactFinderOpen, setIsContactFinderOpen] = useState(false);
+
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const fetchedContactIds = await userService.retrieveUserContacts(userId);
+        
+        // Since fetchedContacts is a list of ids, a list of userDetails must be generated
+        let fetchedContacts: UserDetails[] = [];
+        for (const id of fetchedContactIds) {
+          fetchedContacts.push(await userService.retrieveUserDetails(id));
+        }
+
+        dispatch(setContacts({ contacts: fetchedContacts }));
+      } catch (error: unknown) {
+        console.error(error);
+      }
+    };
+
+    fetchContacts();
+  }, [dispatch, userId]);
+
 
   const addContactButtonPressed = () => setIsContactFinderOpen(true);
 
@@ -24,6 +52,7 @@ function ContactList() {
       </button>
 
       <ul>
+        {contacts.map((c) => c.username)}
       </ul>
     </div>
   );
