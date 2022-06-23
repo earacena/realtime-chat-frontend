5d1f99c6fa5dab316f8cac09a612302bd542dbda
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-23 02:09:24 -0400

    Add icons for different components

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { IoIosMail, IoMdSettings } from 'react-icons/io';
 import { ContactList } from '../Contacts';
 import { Requests } from '../Requests';
 
 function SideBar() {
+
   return (
-    <div className="outline outline-1 h-screen p-1 w-96">
+    <div className="flex flex-col outline outline-1 h-screen p-1 w-96">
+      <IoIosMail size={30} />
+      <BsFillPersonLinesFill  size={30} />
+      <IoMdSettings size={30} />
       <Requests />
       <ContactList />
     </div>
