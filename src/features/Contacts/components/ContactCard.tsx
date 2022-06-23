1885730fdd2c0c59efdb47b0d731d815fd6d6b3a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-23 02:10:43 -0400

    Improve style of ContactCard

--- src/features/Contacts/components/ContactCard.tsx
+++ src/features/Contacts/components/ContactCard.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BsPerson } from 'react-icons/bs';
 import { ContactCardProps } from '../types/contact.types';
 
 function ContactCard({ contactDetails }: ContactCardProps) {
   return (
-    <div>
-      {`${contactDetails.name} (${contactDetails.username})`}
+    <div className="flex border border-1 border-l-0 border-r-0 px-20 border-slate-400 p-2">
+      <div className="outline outline-1 rounded-full mx-3">
+        <BsPerson size={40} className="p-1" />
+      </div>
+      <p className="self-center grow">
+        {contactDetails.name}
+      </p>
     </div>
   )
 }
