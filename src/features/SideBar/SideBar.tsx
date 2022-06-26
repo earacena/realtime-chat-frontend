c9ed338dca42973e3d93cfebfd975b53f322c5ae
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-25 23:44:03 -0400

    Center the three icons in SideBar component evenly

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -8,9 +8,12 @@ function SideBar() {
 
   return (
     <div className="flex flex-col outline outline-1 h-screen p-1 w-96">
-      <IoIosMail size={30} />
-      <BsFillPersonLinesFill  size={30} />
-      <IoMdSettings size={30} />
+      <div className="flex flex-row justify-evenly">
+        <IoIosMail size={30} />
+        <BsFillPersonLinesFill  size={30} />
+        <IoMdSettings size={30} />
+
+      </div>
       <Requests />
       <ContactList />
     </div>
