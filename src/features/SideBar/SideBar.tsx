13d869da7b7a2e50c43ed00595f3a5fd934e7ebc
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-26 01:50:37 -0400

    Implement tab switcher for SideBar component

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -3,19 +3,39 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { IoIosMail, IoMdSettings } from 'react-icons/io';
 import { ContactList } from '../Contacts';
 import { Requests } from '../Requests';
+import { Tab } from '@headlessui/react';
+
+type TabStyleProps = {
+  selected: boolean,
+};
 
 function SideBar() {
-
   return (
     <div className="flex flex-col outline outline-1 h-screen p-1 w-96">
-      <div className="flex flex-row justify-evenly">
-        <IoIosMail size={30} />
-        <BsFillPersonLinesFill  size={30} />
-        <IoMdSettings size={30} />
-
-      </div>
-      <Requests />
-      <ContactList />
+      <Tab.Group>
+        <Tab.List className="flex flex-row justify-evenly">
+          <Tab className={({ selected }: TabStyleProps) => selected ? "bg-slate-500 text-white rounded-lg p-1" : "bg-slate-100 text-black rounded-lg p-1"}>
+            <IoIosMail size={40} />                                                                                                 
+          </Tab>                                                                                                                    
+          <Tab className={({ selected }: TabStyleProps) => selected ? "bg-slate-500 text-white rounded-lg p-1" : "bg-slate-100 text-black rounded-lg p-1"}>
+            <BsFillPersonLinesFill  size={40} />                                                                                    
+          </Tab>                                                                                                                    
+          <Tab className={({ selected }: TabStyleProps) => selected ? "bg-slate-500 text-white rounded-lg p-1" : "bg-slate-100 text-black rounded-lg p-1"}>
+            <IoMdSettings size={40} />
+          </Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            <Requests />
+          </Tab.Panel>
+          <Tab.Panel>
+            <ContactList />
+          </Tab.Panel>
+          <Tab.Panel>
+            <p>Settings coming soon...</p>
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 }
