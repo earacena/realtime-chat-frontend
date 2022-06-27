f58f79d378d2050f6858d0774ad718119cabfbf8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-26 20:01:53 -0400

    Re-export auth state actions

--- src/features/Login/index.ts
+++ src/features/Login/index.ts
@@ -2,5 +2,5 @@ export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './RegisterForm';
 
 export { default as authReducer } from './stores/auth.slice';
-
+export * from './stores/auth.slice';
 export { default as loginService } from './api/login.service';
