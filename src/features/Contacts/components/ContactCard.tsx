8c64d8005e223a80a3f9f41de0bd38a2993d71f1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-26 21:00:53 -0400

    Fix alignment of icon and name in ContactCard

--- src/features/Contacts/components/ContactCard.tsx
+++ src/features/Contacts/components/ContactCard.tsx
@@ -4,11 +4,9 @@ import { ContactCardProps } from '../types/contact.types';
 
 function ContactCard({ contactDetails }: ContactCardProps) {
   return (
-    <div className="flex border border-1 border-l-0 border-r-0 px-20 border-slate-400 p-2">
-      <div className="outline outline-1 rounded-full mx-3">
-        <BsPerson size={40} className="p-1" />
-      </div>
-      <p className="self-center grow">
+    <div className="flex flex-row border border-1 border-l-0 border-t-0 border-r-0 border-slate-400 p-2 w-72">
+      <BsPerson size={40} className="border rounded-full border-gray-400 p-1 mr-3" />
+      <p className="ml-3">
         {contactDetails.name}
       </p>
     </div>
