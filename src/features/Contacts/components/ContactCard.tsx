d6e757b754da02ef32bd72efb32b2f8113e4481f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-29 21:55:28 -0400

    Add hover color and center name vertically

--- src/features/Contacts/components/ContactCard.tsx
+++ src/features/Contacts/components/ContactCard.tsx
@@ -4,7 +4,7 @@ import { ContactCardProps } from '../types/contact.types';
 
 function ContactCard({ contactDetails }: ContactCardProps) {
   return (
-    <div className="flex flex-row border border-1 border-l-0 border-t-0 border-r-0 border-slate-400 p-2 w-72">
+    <div className="flex flex-row border border-1 border-l-0 border-t-0 border-r-0 rounded-lg border-slate-400 shadow p-2 w-64 items-center hover:bg-slate-200">
       <BsPerson size={40} className="border rounded-full border-gray-400 p-1 mr-3" />
       <p className="ml-3">
         {contactDetails.name}
