dea1924a8d38eb45a9291730dc08c2752b34ad74
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-04 00:54:29 -0400

    Implement signaling contacts that user is online

--- src/features/Contacts/components/ContactCard.tsx
+++ src/features/Contacts/components/ContactCard.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BsPerson } from 'react-icons/bs';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { ContactCardProps } from '../types/contact.types';
+import { TbPoint } from 'react-icons/tb';
+import { signalOnline } from '../../Chat';
 
 function ContactCard({ contactDetails }: ContactCardProps) {
+  const dispatch = useAppDispatch();
+  const connectedUsers = useAppSelector((state) => state.users.connectedUsers);
+
+  useEffect(() => {
+    dispatch(signalOnline());
+  }, [dispatch]);
+
+  const isUserOnline = undefined !== connectedUsers.find((users) => users.id === contactDetails.id);
+
+  console.log(connectedUsers);
+
   return (
     <div className="flex flex-row border border-1 border-l-0 border-t-0 border-r-0 rounded-lg border-slate-400 shadow p-2 items-center hover:bg-slate-200 w-72">
       <BsPerson size={40} className="border rounded-full border-gray-400 p-1 mr-3" />
       <p className="ml-3">
         {contactDetails.name}
       </p>
+      { isUserOnline && <TbPoint className="text-green-500" /> }
+      { !isUserOnline && <TbPoint className="text-red-500" /> }
     </div>
   )
 }
