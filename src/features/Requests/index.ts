7970fb5d689fb2be9da290b5b7e3a4a1f3fd7a3a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-05 21:21:43 -0400

    Re-export request slice actions

--- src/features/Requests/index.ts
+++ src/features/Requests/index.ts
@@ -2,4 +2,5 @@ export { default as requestService } from './api/request.service';
 export { default as Requests } from './Requests';
 
 export { default as requestsReducer } from './stores/request.slice';
+export * from './stores/request.slice';
 export { setRequests } from './stores/request.slice';
