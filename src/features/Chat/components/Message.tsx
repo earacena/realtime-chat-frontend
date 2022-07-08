d44b84e476494c914130ed9cbf4c58b0f89072c2
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-08 15:28:45 -0400

    Update MessageProps in Message component

--- src/features/Chat/components/Message.tsx
+++ src/features/Chat/components/Message.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MessageProps } from '../types/chat.types';
 
-function Message({ message, isUserSender }: MessageProps) {
+function Message({ message, isUserSender, isFirstMessage, isLastMessage }: MessageProps) {
   const senderStyle: string = "first:mt-auto m-2 bg-slate-600 text-white p-3 rounded-full self-end shadow";
   const recipientStyle: string = "first:mt-auto m-2 bg-slate-400 text-white p-3 rounded-full self-start shadow";
 
