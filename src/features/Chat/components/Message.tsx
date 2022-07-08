01ff937baa0833176d2cc51131a6b6f0a86fade5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-08 01:28:32 -0400

    Implement Message component

---
+++ src/features/Chat/components/Message.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MessageProps } from '../types/chat.types';
+
+function Message({ message, isUserSender }: MessageProps) {
+  const senderStyle: string = "first:mt-auto m-2 bg-slate-600 text-white p-3 rounded-full self-end shadow";
+  const recipientStyle: string = "first:mt-auto m-2 bg-slate-400 text-white p-3 rounded-full self-start shadow";
+
+  return (
+    <li
+      key={message.id}
+      className={ isUserSender ? senderStyle : recipientStyle }
+    >
+      {message.content}
+    </li>
+  );
+}
+
+export default Message;
