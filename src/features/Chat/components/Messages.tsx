1ff2386570448001b9007a47d08c39b6a57c8645
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-08 15:29:53 -0400

    Update messages selector and update Message props

--- src/features/Chat/components/Messages.tsx
+++ src/features/Chat/components/Messages.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useAppSelector } from '../../../hooks';
+import { selectMessagesByUsernames } from '../stores/chat.slice';
 import Message from './Message';
 
 function Messages() {
-  const messages = useAppSelector((state) => state.chat.messages);
-  const currentRoom = useAppSelector((state) => state.rooms.currentRoom);
+  const messages = useAppSelector(selectMessagesByUsernames);
   const user = useAppSelector((state) => state.auth.user);
 
   return (
     <ul className="flex flex-1 flex-col overflow-auto">
-      {messages.map((m) =>
-        (currentRoom.roomName === m.recipientUsername ||
-          currentRoom.roomName === m.senderUsername) &&
+      {messages.map((m, i) =>
         m.content !== "" &&
-        <Message message={m} isUserSender={m.senderUsername === user.username} />
+        <Message
+          key={m.id}
+          message={m}
+          isUserSender={m.senderUsername === user.username}
+          isFirstMessage={(i > 0) && (messages[i-1].senderUsername !== messages[i].senderUsername)}
+          isLastMessage={(i < messages.length - 1 ) && (messages[i+1].senderUsername !== messages[i].senderUsername)}
+        />
       )}
     </ul>
   );
