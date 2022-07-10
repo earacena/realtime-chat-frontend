c9cd2e6a07ea3ca247f40c1492720db762562df5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-10 01:13:46 -0400

    Add token to fetch headers in retrieveMessages

--- src/features/Chat/api/chat.service.ts
+++ src/features/Chat/api/chat.service.ts
@@ -5,10 +5,19 @@ const baseUrl = 'http://localhost:3001/api/messages';
 type RetrieveMessagesProps = {
   senderUsername: string,
   recipientUsername: string,
+  token: string,
 }
 
-const retrieveMessages = async ({ senderUsername, recipientUsername }: RetrieveMessagesProps) => {
-  const response = await fetch(`${baseUrl}/?senderUsername=${senderUsername}&recipientUsername=${recipientUsername}`);
+const retrieveMessages = async ({ senderUsername, recipientUsername, token }: RetrieveMessagesProps) => {
+  const response = await fetch(
+    `${baseUrl}/?senderUsername=${senderUsername}&recipientUsername=${recipientUsername}`,
+    {
+      headers: {
+        Authorization: `bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
   console.log(response);
   const responseJson = await response.json();
   const messages = MessageArray.check(responseJson);
