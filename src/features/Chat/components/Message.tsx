71820c63e3e35278913e23e6ff1a5d248248ee39
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-15 00:17:36 -0400

    Modify chat bubble style so edges are smoother

--- src/features/Chat/components/Message.tsx
+++ src/features/Chat/components/Message.tsx
@@ -3,9 +3,9 @@ import { MessageProps } from '../types/chat.types';
 
 function Message({ message, isUserSender, isFirstMessage, isLastMessage }: MessageProps) {
 
-  const firstSenderMessageStyle: string = " rounded-tl-full rounded-tr-full rounded-bl-full mb-0";
-  const middleSenderMessageStyle: string = " rounded-tl-full rounded-bl-full mt-1 mb-0";
-  const lastSenderMessageStyle: string = " rounded-bl-full rounded-tl-full rounded-br-full mt-1";
+  const firstSenderMessageStyle: string = " rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl rounded-br mb-0";
+  const middleSenderMessageStyle: string = " rounded-r rounded-tl-3xl rounded-bl-3xl mt-1 mb-0";
+  const lastSenderMessageStyle: string = " rounded-bl-3xl rounded-tl-3xl rounded-br-3xl rounded-tr mt-1";
 
   let senderMessageStyle: string = "";
   if (isFirstMessage && isLastMessage) {
@@ -18,9 +18,9 @@ function Message({ message, isUserSender, isFirstMessage, isLastMessage }: Messa
     senderMessageStyle = middleSenderMessageStyle;
   }
  
-  const firstRecipientMessageStyle: string = " rounded-tr-full rounded-tl-full rounded-br-full mb-0";
-  const middleRecipientMessageStyle: string = " rounded-tr-full rounded-br-full mt-1 mb-0";
-  const lastRecipientMessageStyle: string = " rounded-br-full rounded-tr-full rounded-bl-full mt-1";
+  const firstRecipientMessageStyle: string = " rounded-bl rounded-tr-3xl rounded-tl-3xl rounded-br-3xl mb-0";
+  const middleRecipientMessageStyle: string = " rounded-l rounded-tr-3xl rounded-br-3xl mt-1 mb-0";
+  const lastRecipientMessageStyle: string = " rounded-tl rounded-br-3xl rounded-tr-3xl rounded-bl-3xl mt-1";
 
   let recipientMessageStyle: string = "";
   if (isFirstMessage && isLastMessage) {
