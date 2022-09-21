56b1654c9b16ac712946412e01db9f906b77c3c8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-21 19:42:25 -0400

    Add dark mode theme to SideBar component

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -34,11 +34,12 @@ function SideBar() {
     window.localStorage.removeItem('chatAppUser');
   };
 
-  const tabStyle = "bg-slate-100 text-black rounded-lg p-1 hover:bg-slate-300";
-  const selectedTabStyle = "bg-slate-500 text-white rounded-lg p-1";
+  const tabStyle = "bg-slate-100 text-black rounded-lg p-1 hover:bg-slate-300 dark:bg-slate-600";
+  const selectedTabStyle = "bg-slate-500 dark:bg-slate-400 text-white rounded-lg p-1";
+  const iconStyle = "dark:text-white";
 
   return (
-    <div className="flex flex-col h-screen p-1 w-96 border-r-2">
+    <div className="flex flex-col h-screen p-1 w-96 border-r-2 dark:bg-slate-800 dark:border-slate-600">
       <button className="flex flex-row self-end text-red-500" onClick={handleLogout}>
         <ImExit size={28} />
         Logout
@@ -47,13 +48,13 @@ function SideBar() {
       <Tab.Group>
         <Tab.List className="flex flex-row justify-evenly">
           <Tab className={({ selected }: TabStyleProps) => selected ? selectedTabStyle : tabStyle}>
-            <BsFillPersonLinesFill  size={40} />                                         
+            <BsFillPersonLinesFill className={iconStyle} size={40} />                                         
           </Tab>                                                                         
           <Tab className={({ selected }: TabStyleProps) => selected ? selectedTabStyle : tabStyle}>
-            <IoIosMail size={40} />                                                      
+            <IoIosMail className={iconStyle} size={40} />                                                      
           </Tab>                                                                         
           <Tab className={({ selected }: TabStyleProps) => selected ? selectedTabStyle : tabStyle}>
-            <IoMdSettings size={40} />
+            <IoMdSettings className={iconStyle} size={40} />
           </Tab>
         </Tab.List>
         <Tab.Panels>
