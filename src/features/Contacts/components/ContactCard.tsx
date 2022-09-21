000aa6dafbe953f1dc957b9539e1bae331b5a38a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-21 19:38:32 -0400

    Add dark mode theme for ContactCard component

--- src/features/Contacts/components/ContactCard.tsx
+++ src/features/Contacts/components/ContactCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BsPerson } from "react-icons/bs";
 import { useAppSelector } from "../../../hooks";
 import { ContactCardProps } from "../types/contact.types";
@@ -12,12 +12,12 @@ function ContactCard({ contactDetails }: ContactCardProps) {
     connectedUsers.find((users) => users.id === contactDetails.id);
 
   return (
-    <div className="flex flex-row border border-1 border-l-0 border-t-0 border-r-0 rounded-lg border-slate-400 shadow p-2 items-center hover:bg-slate-200 w-72">
+    <div className="flex flex-row border border-1 border-l-0 border-t-0 border-r-0 rounded-lg border-slate-400 shadow p-2 items-center dark:bg-slate-700 hover:bg-slate-200 w-72 dark:hover:bg-slate-500">
       <BsPerson
         size={40}
-        className="border rounded-full border-gray-400 p-1 mr-3"
+        className="border rounded-full border-gray-400 p-1 mr-3 dark:text-slate-200"
       />
-      <p className="ml-3">{contactDetails.name}</p>
+      <p className="ml-3 dark:text-white">{contactDetails.name}</p>
       {isUserOnline && <VscCircleFilled className="text-green-500" />}
       {!isUserOnline && <VscCircleFilled className="text-red-500" />}
     </div>
